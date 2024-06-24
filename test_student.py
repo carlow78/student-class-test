5bc743f1832ef8ca7decfb992fb0af8ee5ab0786
John Lynch <johnf.lynch@yahoo.ie>
2024-06-24 11:36:32 +0000

    lifecycle method setUp and tearDown

--- test_student.py
+++ test_student.py
@@ -3,18 +3,35 @@ from student import Student
 
 class TestStudent(unittest.TestCase):
 
-    def test_full_name(self):
-        student = Student("John", "Doe")
+    @classmethod
+    def setUpClass(cls):
+        print("setUpClass")
 
+    def setUp(self):
+        print("setUp")
+        self.student = Student("John", "Doe")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("tearDownClass")
+
+    def tearDown(self):
+        print("tearDown")
+
+    def test_full_name(self):
+        print("test_full_name")
+        student = Student("John", "Doe")
         self.assertEqual(student.full_name, "John Doe")
 
     def test_alert_santa(self):
+        print("test_alert_santa")
         student = Student("John", "Doe")
         student.alert_santa()
         self.assertTrue(student.naughty_list)
 
     
     def test_email(self):
+        print("test_email")
         student = Student("John", "Doe")
         self.assertEqual(student.email, "john.doe@email.com")
 
